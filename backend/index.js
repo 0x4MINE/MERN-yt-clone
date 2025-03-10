94b8eb42145c11a1582cf16c442c0bdd969ac8c9
CODEFENCE <xxxcodeshotxxx@gmail.com>
2025-03-10 15:07:40 +0100

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 configDotenv();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
